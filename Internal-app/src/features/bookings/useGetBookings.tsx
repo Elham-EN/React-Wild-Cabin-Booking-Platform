332ab68c8bf28966ccdfa91af8ca930c33c766bc
Elham-EN <ilhamrezaie@gmail.com>
2025-04-19 00:21:42 +1000

    prefetching bookings data for better UX

--- Internal-app/src/features/bookings/useGetBookings.tsx
+++ Internal-app/src/features/bookings/useGetBookings.tsx
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
+import { PAGE_SIZE } from "../../utils/contants";
 
 export type FilterType = {
   field: string;
@@ -16,6 +17,7 @@ export type SortType = {
 
 export function useGetBookings() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // FILTER
   const filterValue = searchParams.get("status");
@@ -57,6 +59,23 @@ export function useGetBookings() {
 
   const bookings = data?.bookingsData;
   const count = data?.count;
+
+  // Pre-Fetching -  load data in the background before a user actually requests it.
+  // The data gets stored in the cache so it's immediately available when needed.
+  const pageTotal = Math.ceil(count! / PAGE_SIZE);
+  if (page < pageTotal) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getAllBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getAllBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { isPending, bookings, count, error };
 }
