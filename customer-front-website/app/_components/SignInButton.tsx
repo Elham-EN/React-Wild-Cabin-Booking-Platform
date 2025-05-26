70f4314d2de8b39b715fff6978a1aebba075639b
Elham-EN <ilhamrezaie@gmail.com>
2025-05-26 23:06:53 +1000

    created custom sign flow

---
+++ customer-front-website/app/_components/SignInButton.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import { signInAction } from "../_libs/action";
+
+function SignInButton(): React.ReactElement {
+  return (
+    <form action={signInAction}>
+      <button
+        className="flex justify-center items-center gap-6 text-lg border 
+        cursor-pointer border-primary-300 px-10 py-4 font-medium"
+      >
+        <Image
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span className="mt-1">Continue with Google</span>
+      </button>
+    </form>
+  );
+}
+
+export default SignInButton;
--- customer-front-website/app/account/page.tsx
+++ customer-front-website/app/account/page.tsx
@@ -1,15 +1,17 @@
 import { Metadata } from "next";
 import React from "react";
+import { auth } from "../_libs/auth";
 
 export const metadata: Metadata = {
   title: "Account",
 };
 
 // Server-side Rendered Component
-export default function Page(): React.ReactElement {
+export default async function Page(): Promise<React.ReactElement> {
+  const session = await auth();
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Jonas
+      Welcome, {session?.user?.name}
     </h2>
   );
 }
