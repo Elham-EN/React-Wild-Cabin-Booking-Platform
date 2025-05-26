5aed0e2a89eb2d5fe4d925030b0ed767100e9066
Elham-EN <ilhamrezaie@gmail.com>
2025-05-26 22:46:02 +1000

    protecting routes with next.js middleware

--- customer-front-website/app/_libs/auth.ts
+++ customer-front-website/app/_libs/auth.ts
@@ -8,4 +8,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
   ],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // If return true, then the current user is authorized to go
+      // through onto that route that is being protected
+      return !!auth;
+    },
+  },
 });
